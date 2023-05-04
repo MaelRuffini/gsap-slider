67be5433837f44d08533ac887229353a9f68c550
MaelRuffini <mael.lucien.ruffini@gmail.com>
2023-05-04 08:47:39 +0200

    slider setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,60 @@
 import './styles/style.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import Lenis from '@studio-freight/lenis'
 
-console.log('Hello')
+gsap.registerPlugin(ScrollTrigger)
+
+/**
+ * Lenis scroll
+ */
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+requestAnimationFrame(raf)
+
+
+/**
+ * Gsap animations
+ */
+const items = document.querySelectorAll('.slider__item')
+const height = document.querySelector('.slider').offsetWidth
+const width = (items.length - 1) * 100
+
+document.querySelector('.sticky__wrapper').style.height = `${height / 16}rem`
+
+gsap.to('.slider__item', {
+    xPercent: -width,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.sticky__wrapper',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: true
+    }
+})
+
+items.forEach(item => {
+
+    gsap.to(item.querySelector('.slider__image'), {
+        xPercent: 50,
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.sticky__wrapper',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: true
+        }
+    })
+
+})
